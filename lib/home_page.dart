9dfb6f7a12c53506e1edf1bf53a839cc8396ab34
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-22 07:05:50 +0200

    Button to send notifications

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,7 +10,7 @@ class HomePage extends StatelessWidget {
         child: ElevatedButton(
             onPressed: () {},
             child: Text(
-              'Helloworld',
+              'Send Notifications',
             )),
       ),
     );
