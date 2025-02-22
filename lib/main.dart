6d2ab39fe9cd52aceaa7eb52a740a3e4775ef3db
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-22 07:02:05 +0200

    Removed all code in widget tests

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,1 @@
-import 'package:flutter/material.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  runApp(DemoApp());
-}
-
-class DemoApp extends StatelessWidget {
-  const DemoApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
