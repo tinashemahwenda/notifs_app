b8949278ac1802988a2e6091263b38a09e2f16ce
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-22 07:05:16 +0200

    Set homepage as the app main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:notifs_app/home_page.dart';
 
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(DemoApp());
+}
+
+class DemoApp extends StatelessWidget {
+  const DemoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
