b93009ab016299113c67040b91b104f098687b5d
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-22 07:11:47 +0200

    Created settings for initializing the notification

--- lib/noti_service.dart
+++ lib/noti_service.dart
@@ -7,5 +7,12 @@ class NotiService {
 
   bool get isInitialized => _isInitialized;
 
-  Future<void> initNotifications() async {}
+  Future<void> initNotifications() async {
+    if (_isInitialized) return;
+
+    const initSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    const initSettings = InitializationSettings(android: initSettingsAndroid);
+  }
 }
