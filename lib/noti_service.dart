180e47a59292b5e6d395be524259416cedad3175
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-22 07:08:19 +0200

    Cretaed a notiServices class

---
+++ lib/noti_service.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotiService {
+  final notificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  bool _isInitialized = false;
+
+  bool get isInitialized => _isInitialized;
+}
